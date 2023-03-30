bca66e1597999f2fe774a616a843f6dbe8cbd0d0
Michael <michael.kuhn@liveunitedchicago.org>
2023-03-30 11:38:35 -0500

    functional website

--- src/js/submit.js
+++ src/js/submit.js
@@ -1,51 +1,62 @@
-
-
-
-// document.getElementById("submit")
-//     .addEventListener("click", submitIt);
-
     
 document.getElementById("form")
     .addEventListener("submit", submitIt);
 
-// console.log("test")
-const output = () => 
-    document.getElementById("output").children[0];
-console.log(output())
-// output().innerHTML = "test"
-
+const output = document.getElementById("output").children[0]
 
 function submitIt(e){
     e.preventDefault();
     let input = document.getElementById("url")
     let url = input.value;
     input.value = "";
+    output.innerHTML = ""
     console.log(url);
     archiveIt(url)
 }
 
 async function archiveIt(url){
-    let snapshots;
 
-    checkAvail(url)
-        .then((respJson) => {
-            if (respJson == '') {
-                output().innerHTML = "An Error Has Occured";
-            } else {
-
+    try {
+        await fetch(url, {
+            mode: 'no-cors'
+        });
+        
+        checkAvail(url)
+            .then((respJson) => {
                 console.log(respJson)
-                console.log(respJson.archived_snapshots)
-            }
-        })
+                let snp = respJson.archived_snapshots
+                let link = document.createElement("a");
+                link.target = "_blank"
+                link.rel = "noreferrer"
+
+                if (snp.hasOwnProperty("closest")) {
+                    link.href = snp.closest.url;
+                    link.innerText = link.href;
+                    outputMsg = "That URL is archived. Here is the most recent archive URL: <br>";                    
+                } else {
+                    link.href = "https://web.archive.org/save/" + url;
+                    link.innerText = link.href
+                    outputMsg = "That URL is not archived. Please archive it using the link below: <br> <em>Note: archive.org may be overloaded, in that case try archive.is</em><br><br>";
+                    let isLink = document.createElement("a");
+                    isLink.href = "https://archive.is"
+                    isLink.innerHTML = "<br><br>" + isLink.href
+
+                    link.appendChild(isLink)
+                }
+                output.innerHTML = outputMsg;
+                output.appendChild(link);
+            })
+    } catch (e) {
+        console.log(e)
+        errMsg = "An error has occurred, unable to process: " + url;
+        output.innerHTML = errMsg;
+    }
+    
 }
 
 async function checkAvail(url = "") {
     let aurl = "https://archive.org/wayback/available?url="
-
-    // let rsp
     console.log('furl', aurl+url)
-
-    fetch(url)
 
     return fetch(aurl + url)
         .then((resp) => {
@@ -53,19 +64,5 @@ async function checkAvail(url = "") {
                 return resp.json()
             }
             throw new Error(resp)
-        })
-        // .then((respJson) => {rsp = respJson})
-        .catch((error) => {
-            console.log("error", error)
-            return ''
         });
-    
-    
-    // console.log('rsp', rsp)
-    
-    // return rsp
-}
-
-function saveIt(url){
-    console.log('save ', url)
 }
