0f10307528bbad4b6f15e64b4da36db8546796fe
Michael Kuhn <mickuhn95@gmail.com>
2023-03-29 21:54:31 -0500

    fetch progress

--- src/js/submit.js
+++ src/js/submit.js
@@ -2,33 +2,47 @@
 
 
 document.getElementById("submit")
-    .addEventListener("click", validateIt);
+    .addEventListener("click", submitIt);
+console.log("test")
 
-
-function archiveIt(url){
-    console.log(url)
-}
-
-
-function validateIt(e){
-    console.log(e);
+function submitIt(e){
     let url = document.getElementById("url").value;
     console.log(url);
-   if(isValidHttpUrl(url)){
-        archiveIt(url)
-   } else {
-       console.log("invalid url: ", url)
-   }
+    archiveIt(url)
 }
 
+async function archiveIt(url){
+    let snapshots;
 
+    await checkAvail(url)
+        .then((rsp)=>{rsp.archived_snapshots})
+        .then((result)=>{
+            console.log(rsp, result)
+            snapshots = result
+            if(snapshots == {}){
+                saveIt(url)
+            } else {
+                console.log(snapshots.closest.url)
+            }
+        })
+        .catch((error) => console.log("error", error))
+}
 
-function isValidHttpUrl(string) {
-    let url;
-    try {
-      url = new URL(string);
-    } catch (_) {
-      return false;
-    }
-    return url.protocol === "http:" || url.protocol === "https:";
-  }
+async function checkAvail(url = "") {
+    let aurl = "https://archive.org/wayback/available?url="
+
+    let rsp
+
+    const response = await fetch(aurl + url)
+        .then((data) => {rsp = data.json()})
+        .catch((error) => console.log("error", error));
+    
+    
+    console.log(rsp)
+    
+    return rsp
+}
+
+function saveIt(url){
+    console.log('save ', url)
+}
